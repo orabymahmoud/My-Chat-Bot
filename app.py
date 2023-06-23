fa5cf5d2f0aea9e7b206eac66efd630b63553239
Oraby Mahmoud <orabymahmoud@yahoo.com>
2023-06-24 02:40:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from backend import app
-
+from flask_cors import CORS
 # if __name__ == "__main__":
 #     app.run(host='0.0.0.0', debug=True)
 if __name__ == '__main__':  # Script executed directly?
-    print("songs application.")
+    CORS(app, resources={r"/*": {"origins": ["https://asstoken2.github.io", "http://localhost:3000"]}})
     app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
