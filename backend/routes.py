12735a8e456b3ccfd4cd8e02a8a5271c4233a0b0
Oraby Mahmoud <orabymahmoud@yahoo.com>
2023-06-24 15:43:54 +0300

    Update routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -9,6 +9,7 @@ from pymongo.errors import OperationFailure
 from pymongo.results import InsertOneResult
 from bson.objectid import ObjectId
 import sys
+from datetime import datetime
 
 # Get the path of the subdirectory containing the chatterbot package
 chatterbot_dir = os.path.join(os.path.dirname(__file__), 'chatterbot')
@@ -37,18 +38,43 @@ def bot_talk():
     if 'message' in data and data['message'] is not None:
         message = data['message'] 
         if message is not None:
-            return {"Response": str(Bot_Chat(message))}, 200
+            return {
+                "Response": str(Bot_Chat(message)) ,
+                "Status": 'Success',
+                "ResultCode": '0',
+                "RowVersion": datetime.now()
+             }, 200
         else:
-            return {"Response": 'The message is missing'}, 300
+            return {
+                "Response": 'The message is missing' ,
+                "Status": 'Failed',
+                "ResultCode": '1',
+                "RowVersion": datetime.now()
+                }, 300
     elif 'train_bot' in data and data['train_bot'] is not None:
         train_bot = data['train_bot']
         if train_bot == 'True':
             Bot_Train()
-            return {"Response": 'Bot have trained Succesfully'}, 200
+            return {
+                "Response": 'Bot have trained Succesfully',
+                "Status": 'Success',
+                "ResultCode": '0',
+                "RowVersion": datetime.now()
+                }, 200
         else:
-            return {"Response": 'Bot have not trained'}, 300
+            return {
+                "Response": 'Bot have not trained',
+                "Status": 'Failed',
+                "ResultCode": '1',
+                "RowVersion": datetime.now()
+                }, 300
     else:
-        return {"Response": 'Body is empty'}, 300
+        return {
+                "Response": 'Body is empty',
+                "Status": 'Failed',
+                "ResultCode": '1',
+                "RowVersion": datetime.now()
+                }, 300
 
 
 
@@ -60,9 +86,22 @@ def bot_train():
         response = data['response'] 
         if message is not None:
             Bot_Train_Specific(message , response)
-            return {"Response": 'Bot have trained Succesfully'}, 200
+            return {
+                "Response": 'Bot have trained Succesfully',
+                "Status": 'Success',
+                "ResultCode": '0',
+                "RowVersion": datetime.now()
+                }, 200
         else:
-            return {"Response": 'The message is missing'}, 300
+            return {
+                "Response": 'The message is missing',
+                "Status": 'Failed',
+                "ResultCode": '1',
+                "RowVersion": datetime.now()}, 300
     
     else:
-        return {"Response": 'Body is Invalid'}, 300
+        return {
+                "Response": 'Body is Invalid',
+                "Status": 'Failed',
+                "ResultCode": '1',
+                "RowVersion": datetime.now()}, 300
