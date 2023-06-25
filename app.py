043a5302e65138983128a8f71c146a9a30077c1c
Oraby Mahmoud <orabymahmoud@yahoo.com>
2023-06-25 13:10:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ from flask_cors import CORS
 #     app.run(host='0.0.0.0', debug=True)
 if __name__ == '__main__':  # Script executed directly?
     CORS(app, resources={r"/*": {"origins": ["https://asstoken2.github.io", "http://localhost:3000"],
-        "methods": ["GET", "POST", "DELETE", "PUT"],
-        "allow_headers": ["Authorization", "Content-Type"]}})
+        "methods": ["GET", "POST", "DELETE", "PUT"]}})
     app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
