29b0724f67d1cff5982a5f0366ae06aec8ad15a0
Oraby Mahmoud <orabymahmoud@yahoo.com>
2023-06-26 01:45:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from backend import app
 from flask_cors import CORS
-CORS(app, resources={r"/*": {"origins": ["https://asstoken2.github.io", "http://localhost:3000"]}})
+CORS(app, resources={r"/*": {"origins": ["https://asstoken2.github.io", "http://localhost:3000"]}},supports_credentials=True)
 # if __name__ == "__main__":
 #     app.run(host='0.0.0.0', debug=True)
 if __name__ == '__main__':  # Script executed directly?
